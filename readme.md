ee94ff5130e7cf868d3a383f0e7939b096da9ef4
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-10 11:22:59 +1000

    Interpolation

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 1.install vue environment 'npm install -g @vue/cli'
 2.create project 'vue create name'
 3.execute projecy 'npm run serve'
+
+#template syntax
+1.Interpolation
+  v-html == innerHtml
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,33 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>{{message}}</p> <!--binding text-->
+    <div v-html="mingkeLink"></div> <!--binding html-->
+    <p v-bind:class="onActive">Highlight</p> <!--binding class attribute-->
+    <p v-bind:id="vid">BindingID</p> <!--binding class id-->
+    <input :disabled="isAbled" value="Bool Attribute Disabled"/> <!--binding boolean attribute-->
+    <div v-bind="objectOfAttrs">Multiple Binding Dynamic</div> <!--binding multiple attributes-->
+    <p>{{number>10?"Number is bigger than 10":"Number isn't bigger than 10"}}</p> <!--Expression only not statement-->
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){ //declare a data source
+    return{
+      message:'Text:hello Vue from Mingke',
+      mingkeLink:'<a href="https://github.com/Mingke1999">MingkeGit</a>',
+      onActive:'active',
+      vid:'007',
+      isAbled:'false',
+      objectOfAttrs: {id: 'container',class: 'wrapper'},
+      number:10,
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
